55cab1f7a54ec3512b8cda3e52e183ee63c8ddf6
Noman <mdnoman70499@gmail.com>
2023-10-10 18:54:32 +0530

    sqrt qeustion added

---
+++ problems easy/sqrttt.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int root(int n){
+    return sqrt(n);
+}
+int main()
+{
+    int a;
+    cin >> a;
+    cout << root(a);
+
+    return 0;
+}
